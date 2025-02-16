40432a457c0fb4b299e2ece70ec06f7f2b8099ed
himanshu <tyagihimanshu1910@gmail.com>
2025-02-16 11:24:23 -0800

    add error-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { ContactUsHeaderComponent } from './Components/contact-us-header/contact
 import { LoginPageComponent } from './Components/login-page/login-page.component';
 import { SignUpPageComponent } from './Components/sign-up-page/sign-up-page.component';
 import { SemesterPageComponent } from './Components/semester-page/semester-page.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { ErrorPageComponent } from './Components/error-page/error-page.component';
 
 
 export const routes: Routes = [
@@ -16,12 +18,14 @@ export const routes: Routes = [
     {path:'Contact_us',component:ContactUsComponent},
     {path:'About_us',component:AboutUsComponent},
     {path:'Header',component:ContactUsHeaderComponent},
-    // {path:'Footer',component:FooterComponent},
+    {path:'Footer',component:FooterComponent},
     {path:'Login-page',component:LoginPageComponent},
     {path:'Signup-page',component:SignUpPageComponent},
     {path:'Semester-page',component:SemesterPageComponent},
+    {path:'Error-page',component:ErrorPageComponent},
 
 
+    { path: '**', component: ErrorPageComponent }
 
-    { path: '**', redirectTo: '', pathMatch: 'full' }
+    // { path: '**', redirectTo: 'ErrorPageComponent', pathMatch: 'full' }
 ];
